ff44ecaab90c9ecd09a24858c2372958724ace1a
Labina <labina.stamenkova@gmail.com>
2022-05-16 21:47:13 +0200

    Created CRUD

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy 
 from flask_marshmallow import Marshmallow
 from dotenv import load_dotenv
+from json import JSONEncoder
+import psycopg2
 import os
+
 
 #Init app
 app = Flask(__name__)
@@ -21,11 +24,163 @@ db_name = os.getenv('POSTGRES_DB')
 connection_url = 'postgresql+psycopg2://{username}:{password}@{url}/{name}'.format(username=db_username, password=db_password, url=db_url, name=db_name)
 app.config['SQLALCHEMY_DATABASE_URI'] = connection_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+con = psycopg2.connect(
+    user = db_username,
+    password = db_password,
+    host = 'localhost',
+    database = db_name
+)
 
 #Init DB
 db = SQLAlchemy(app)
 #Init MA
 ma = Marshmallow(app)
+
+#Import models and schemas
+#from models import Resource, BookResource, Appointment, ResourceType, ResourceDto, BookResourceDto, AppointmentDto
+#from models import resource_schema, resources_schema, appointemts_schema, appointment_schema, bookResource_schema, bookResources_schema
+
+
+#createResource
+@app.route('/resource', methods=['POST'])
+def create_resource():
+    import models
+
+    name = request.json['name']
+    type = request.json['type']
+
+    new_resource = models.Resource(name, type)
+
+    db.session.add(new_resource)
+    db.session.commit()
+
+    return models.resource_schema.jsonify(new_resource)
+
+
+#editResource
+@app.route('/resource/<id>', methods=['PUT'])
+def edit_resource(id):
+    import models
+
+    resource = models.Resource.query.get(id)
+
+    name = request.json['name']
+    type = request.json['type']
+
+    resource.name = name
+    resource.type = type
+
+    db.session.commit()
+
+    return models.resource_schema.jsonify(resource.serialize)
+
+
+#deleteResource
+@app.route('/resource/<id>', methods=['DELETE'])
+def delete_resource(id):
+    import models
+
+    resource = models.Resource.query.get(id)
+
+    db.session.delete(resource)
+    db.session.commit()
+
+    return models.resource_schema.jsonify(resource.serialize)
+
+
+#getResource
+@app.route('/resource/<id>', methods=['GET'])
+def get_resource(id):
+    import models
+
+    resource = models.Resource.query.get(id)
+    
+    return models.resource_schema.jsonify(resource.serialize)
+
+
+#getResources
+@app.route('/resources', methods=['GET'])
+def get_resources(id):
+    import models
+
+    resources = models.Resource.query.all()
+    result = models.resources_schema.dump([resource.serialize for resource in resources])
+
+    return jsonify(result)
+
+
+#bookAppointment
+@app.route('/appointment', methods=['POST'])
+def book_appointment():
+    import models
+
+    date_from = request.json['date_from']
+    date_to = request.json['date_to']
+    resources = request.json['appointments']
+    
+    appointment = models.Appointment(date_from, date_to, resources)
+    models.bookResource_schema.dump(resources)
+
+    db.session.add(appointment)
+    db.session.commit()
+
+    return models.appointment_schema.jsonify(appointment)
+
+
+#editAppointment
+@app.route('/appointment/<id>', methods=['PUT'])
+def edit_appointment(id):
+    import models
+
+    appointment = models.Appointment.query.get(id)
+    date_from = request.json['date_from']
+    date_to = request.json['date_to']
+    resources = request.json['appointments']
+
+    appointment.date_from = date_from
+    appointment.date_to = date_to
+    appointment.resources = resources
+
+    models.bookResource_schema.dump(resources)
+
+    db.session.commit()
+
+    return models.appointment_schema.jsonify(appointment.serialize)
+
+
+#cancelAppointment
+@app.route('/appointment/<id>', methods=['DELETE'])
+def cancel_appointment(id):
+    import models
+
+    appointment = models.Appointment.query.get(id)
+
+    db.session.delete(appointment)
+    db.session.commit()
+
+    return models.appointment_schema.jsonify(appointment)
+
+
+#getAppointment
+@app.route('/appointment/<id>', methods=['GET'])
+def get_appointment(id):
+    import models
+
+    appointment = models.Appointment.query.get(id)
+
+    return models.appointment_schema.jsonify(appointment.serialize)
+
+
+
+#getAppointments
+@app.route('/appointments', methods=['GET'])
+def get_appointments(id):
+    import models
+
+    appointments = models.Appointment.query.all()
+    result = models.appointemts_schema.dump([appointment.serialize for appointment in appointments])
+
+    return jsonify(result)
 
 
 #Run server 
--- models.py
+++ models.py
@@ -1,46 +1,118 @@
-import re
-import resource
-from app import db
-from enum import Enum
+from xmlrpc.client import DateTime
+import app
+from dataclasses import dataclass
+import enum
+import sqlalchemy
 
-class ResourceType(Enum):
+
+class ResourceType(enum.Enum):
     ROOM = 1
     FOOD = 2
     TOYS = 3
     MEDICATION = 4
 
-#id, name, resourcetype
-class Resource(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    type = db.Column(Enum(ResourceType))
+resource_type = app.db.Enum(ResourceType, name='resource_type_enum')
+
+class Resource(app.db.Model):
+    id = app.db.Column(app.db.Integer, primary_key=True)
+    name = app.db.Column(app.db.String(100), nullable=False)
+    type = app.db.Column(resource_type, default=ResourceType.FOOD)
 
     def __init__(self, name, type):
         self.name = name
         self.type = type
 
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'type': self.type
+        }
 
-class BookResource(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    resource_id = db.Column(db.Integer, db.ForeignKey('resource.id'), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
+#Schema
+class ResourceSchema(app.ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'type')
+
+resource_schema = ResourceSchema()
+resources_schema = ResourceSchema(many=True)
+
+
+class BookResource(app.db.Model):
+    id = app.db.Column(app.db.Integer, primary_key=True)
+    resource_id = app.db.Column(app.db.Integer, app.db.ForeignKey('resource.id'), nullable=False)
+    appointment_id = app.db.Column(app.db.Integer, app.db.ForeignKey('appointment.id'), nullable=False)
+    quantity = app.db.Column(app.db.Integer, nullable=False)
 
 
     def __init__(self, resource_id, quantity):
         self.resource_id = resource_id
         self.quantity = quantity
 
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'resource_id': self.resource_id,
+            'quantity': self.quantity
+        }
 
-class Appointment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_from = db.Column(db.DateTime, nullable=False)
-    date_to = db.Column(db.DateTime, nullable=False)
-    resources = db.relationship('BookResource', backref='appointment', lazy=True)
+#Schema
+class BookResourceSchema(app.ma.Schema):
+    class Meta:
+        fields = ('id', 'resource_id', 'quantity')
+
+bookResource_schema = BookResourceSchema()
+bookResources_schema = BookResourceSchema(many=True)
+
+
+class Appointment(app.db.Model):
+    id = app.db.Column(app.db.Integer, primary_key=True)
+    date_from = app.db.Column(app.db.DateTime, nullable=False)
+    date_to = app.db.Column(app.db.DateTime, nullable=False)
+    resources = app.db.relationship('BookResource', backref='appointment', lazy=True)
 
     def __init__(self, date_from, date_to, resources = list()):
         self.date_from = date_from
         self.date_to = date_to
         self.resources = resources
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'date_from': self.date_from,
+            'date_to': self.date_to,
+            'resources': [resource.serialize for resource in self.resources]
+        }
+
+#Schema
+class AppointmentSchema(app.ma.Schema):
+    class Meta:
+        fields = ('id', 'date_from', 'date_to', 'resources')
+
+appointment_schema = AppointmentSchema()
+appointemts_schema = AppointmentSchema(many=True)
+
+
+@dataclass
+class ResourceDto:
+    name: str
+    resourceType: ResourceType
+
+@dataclass
+class BookResourceDto:
+    resourceId: int
+    quantity: int
+
+@dataclass
+class AppointmentDto:
+    date_from: DateTime
+    date_to: DateTime
+    resources: list()
+
+
 
 
 
